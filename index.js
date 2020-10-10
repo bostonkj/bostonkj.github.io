bf23b8d7d97930dc4acadaa7dcc9271176974a7b
bak45247 <bak45247@bethel.edu>
2020-10-10 02:52:20 -0500

    more issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var profanityList = ["go fuck yourself", "die", "please end yourself", "you suck
                     "you're a dickhead", "son of a bitch", "you fucking cunt", "la concha de la lora"];
 
 function displayProfanity(){
-    document.getElementById("profanityDisplay").innerHTML(Math.floor(Math.random() * profanityList.length));
+    document.getElementById("profanityDisplay") = innerHTML(Math.floor(Math.random() * profanityList.length));
 }
 
 window.onload = displayProfanity;
