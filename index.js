dce58399ca528d9203378a077a1489a8f8c261bf
bak45247 <bak45247@bethel.edu>
2020-10-10 03:12:22 -0500

    added more profanity

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var profanityList = ["go fuck yourself", "die", "please end yourself", "you suck", "piss off", 
-                    "you're a dickhead", "son of a bitch", "you fucking cunt", "la concha de la lora"];
+                    "you're a dickhead", "son of a bitch", "you fucking cunt", "la concha de la lora",
+                    "you suck", "we tolerate you", "no one cares if you're alive", "cease living", "commit die"];
 
 function displayProfanity(){
     document.getElementById("profanityDisplay").innerHTML = profanityList[Math.floor(Math.random() * profanityList.length)];
